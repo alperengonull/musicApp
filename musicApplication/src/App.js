0cd035aa395f70b51fddd788e70ed6bdbdb001b9
alperengonull <81569125+alperengonull@users.noreply.github.com>
2023-03-30 21:28:17 +0300

    Created some components
    I created SongCard components, there will be some information about music title,artist,year,image etc.

--- musicApplication/src/App.js
+++ musicApplication/src/App.js
@@ -1,12 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text,StyleSheet,FlatList } from 'react-native'
 import React from 'react'
+import music_data from './music-data.json'
+import SongCard from './components/SongCard'
 
 const App = () => {
+
+  _renderItem = ({item}) => <SongCard song={item} />
+  // <Text> {item.title} </Text>
+
   return (
-    <View>
-      <Text>App</Text>
+    <View style={styles.container}>
+       <View style={styles.container}>
+      <FlatList
+      keyExtractor={item => item.id}
+      data={music_data}
+      renderItem={_renderItem}
+      />
+    </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  }
+})
+
 
 export default App;
